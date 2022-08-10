f5c8c99087b5d862017d33f3e3f28de4979bab65
Denis-AAA <104863818+Denis-AAA@users.noreply.github.com>
2022-08-10 16:28:03 +0300

    Initial commit

---
+++ menu.inc.php
@@ -0,0 +1,9 @@
+<nav>
+    <ul>
+        <li><a href="#"> Abouth My </a></li>
+        <li><a href="#"> My Portfolio </a></li>
+        <li><a href="#"> My Articles </a></li>
+        <li><a href="#"> Links </a></li>
+        <li><a href="#"> Contacts </a></li>
+    </ul>
+</nav>
